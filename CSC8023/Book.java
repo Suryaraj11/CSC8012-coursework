208389d53a165eb2afebb1b6bf07323c51c9847c
C5020545 <c5020545@newcastle.ac.uk>
2025-10-06 13:15:29 +0100

    First class

--- CSC8023/Book.java
+++ CSC8023/Book.java
@@ -1,4 +1,23 @@
 package CSC8023;
 
 public class Book {
+
+    private String bookStoreName;
+    private String bookTitle;
+
+    public String getBookStoreName() {
+        return bookStoreName;
+    }
+
+    public void setBookStoreName(String bookStoreName) {
+        this.bookStoreName = bookStoreName;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
 }
