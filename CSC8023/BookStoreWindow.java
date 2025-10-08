cdd8e6037d86019059311fc6f0a879a8b2ff62bd
Suryaraj11 <C5020545@newcastle.ac.uk>
2025-10-08 17:39:20 +0100

    Final BookStoreWindow

--- CSC8023/BookStoreWindow.java
+++ CSC8023/BookStoreWindow.java
@@ -1,35 +1,62 @@
 package CSC8023;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class BookStoreWindow {
 
     public static void main(String[] args) throws Exception {
         try {
-
-            System.out.println("Welcome to the Book Store! \nkindly choose one of the book store options below:");
+            DecimalFormat df = new DecimalFormat("0.00");
+            System.out.println("Welcome to the Book Store!");
             Scanner sc = new Scanner(System.in);
 
             BookStoreWindowIO bookStore = new BookStoreWindowIO();
+            while (true) {
+                System.out.print("kindly choose one of the book store options below:\n");
+                HashMap<Integer, String> namesList = bookStore.getStoresList();
+                int i = 0;
+                for (i=0; i < namesList.size(); i++) {
+                    String name = namesList.get(i + 1);
+                    System.out.println(i + 1 + ". " + name);
+                }
+                System.out.println(i+1+". Quit");
+                int bookStoreName = 0;
+                try{
+                    bookStoreName = sc.nextInt();
+                }
+                catch(Exception e){
+                    int size = namesList.size() + 1;
+                    System.out.println("\nPlease select an option(Number) which is within the range of 1-"+size);
+                    sc.nextLine();
+                    continue;
+                }
+                if(bookStoreName<1 || bookStoreName>namesList.size()+1){
+                    int size = namesList.size() + 1;
+                    System.out.println("\nPlease select an option which is within the range of 1-"+size);
+                    continue;
+                }
+                if(bookStoreName == i+1) {
+                    System.out.println("\nThank you for visiting our Book Store!");
+                    break;
+                }
+                List<Book> booksList = bookStore.bookStoreToBooks.get(namesList.get(bookStoreName));
+                System.out.println("Boook shop name: " + namesList.get(bookStoreName));
+                for (int j = 0; j < booksList.size(); j++) {
+                    Book book = booksList.get(j);
+                    System.out.println(book.toString());
+                }
 
-            HashMap<Integer, String> namesList = bookStore.getStoresList();
-            for(int i=0;i<namesList.size();i++){
-                String name = namesList.get(i+1);
-                System.out.println(i+1 + ". "+name);
-            }
-            int bookStoreName = sc.nextInt();
-            List<Book> booksList = bookStore.bookStoreToBooks.get(namesList.get(bookStoreName));
-            System.out.println("Boook shop name: "+namesList.get(bookStoreName));
-            for(int i=0;i<booksList.size();i++){
-                Book book = booksList.get(i);
-                System.out.println(book.toString());
+                System.out.println("\nThe statistics on highest value, oldest book, and average value of the books:");
+                bookStore.getBooksStatistics(namesList.get(bookStoreName));
+                System.out.println("Highest value book: " + bookStore.highValueBook.getBookTitle() + " (published " + bookStore.highValueBook.getPublishedYear() + "), £" + df.format(bookStore.highValueBook.getValue()));
+                System.out.println("Oldest book: " + bookStore.oldestBook.getBookTitle() + " (published " + bookStore.oldestBook.getPublishedYear() + ")");
+                System.out.println("Average value of books: £" + df.format(bookStore.getAverageValue())+"\n");
             }
         }
         catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Sorry for the inconvenience, kindly restart the program.");
         }
     }
+
 }
